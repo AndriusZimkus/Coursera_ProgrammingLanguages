bdc0ea986c8f1bad5c4ae2cce10284ce844c179b
Andrius Žimkus <and.zimkus@gmail.com>
2024-09-29 13:02:38 +0300

    2024-09-29 1

--- Part B/Module 2/RacketBasics5.rkt
+++ Part B/Module 2/RacketBasics5.rkt
@@ -8,3 +8,26 @@
 ; (define f 17) ; doubled definition
 
 ;(define (+ x y) (- x (* -1 y) (* -1 1))) ; redefined + - poor style
+
+
+; assignment statements - set!
+(define b2 3)
+(define f2 (lambda (x) (* 1 (+ x b2))))
+(define c2 (+ b2 4)) ; 7
+(set! b2 5)
+(define z2 (f2 4))   ; 9
+(define w2 c2)       ; 7
+
+; A general principle:
+; If something you need not to change might change
+; make a local copy of it.
+
+(define b3 3)
+(define f3
+  (let ([b3 b3])
+    (lambda (x) (* 1 (+ x b3)))))
+(set! b3 4)
+
+; Defense against mutation:
+; 1. Do not allow mutation
+; 2. Mutable top-level bindings a highly dubious idea
