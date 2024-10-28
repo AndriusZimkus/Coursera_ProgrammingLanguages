c8912554d8ed00316995e062f9f98d196b03643c
skuller99 <and.zimkus@gmail.com>
2024-10-28 21:00:48 +0200

    2024-10-28

---
+++ Part C/Module 8/Lecture notes/2_ObjectState.rb
@@ -0,0 +1,30 @@
+class A
+
+  Global = 9
+  
+  #constructor
+  def initialize(f=0)
+    @foo = f
+  end
+  def m1
+    @foo = 0
+  end
+
+  def m2 x
+    @foo += x
+    @@bar += 1
+  end
+
+  def foo
+    @foo
+  end
+
+  def self.reset_bar
+    @@bar = 0
+  end
+
+  def bar
+    @@bar
+  end
+
+end
