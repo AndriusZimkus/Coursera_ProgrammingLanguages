8aa3ca2a1949478d29664a4584a7ba5ce4b116a8
Andrius Žimkus <and.zimkus@gmail.com>
2024-10-30 21:07:35 +0200

    Class definitions are dynamic

---
+++ Part C/Module 8/Lecture notes/5.rb
@@ -0,0 +1,37 @@
+x = if not nil and not false then 5 else 3 end
+puts x
+
+#puts x.methods
+#puts 3.methods - nil.methods
+
+#Load My_rational
+load "4.rb"
+
+x = MyRational.new(9,6)
+puts x.to_s
+
+class MyRational
+  def double
+    self + self
+  end
+end
+
+# x also changed
+puts x.double.double
+
+puts 3.class
+
+class Fixnum
+  def double
+    self + self
+  end
+end
+
+puts 3.double
+
+public
+def m
+  42
+end
+
+puts 0.m
