342c5ed4d18f34ebadc2c7036bc08a0002485b38
skuller99 <and.zimkus@gmail.com>
2024-11-05 22:11:35 +0200

    2024-11-05 22:11:35

---
+++ Part C/Module 8/Lecture notes/12.rb
@@ -0,0 +1,10 @@
+#Dynamic dispatch
+
+#Self object
+
+#@x - self - instance variables
+#@@x - self.class - class variables
+
+#Method calls start from environment where object is self
+
+#Static overloading
